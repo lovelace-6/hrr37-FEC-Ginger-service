8db1c73cda8cc0f90db060278d326242a170e7fc
Ginger Barham <gingermostly@gmail.com>
2019-03-28 12:05:26 -0700

    Setup basic express server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hi there');
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`))
