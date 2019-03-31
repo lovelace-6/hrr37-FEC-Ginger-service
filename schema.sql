ff4d8a45a67882a5ad4a7c1a8db196437846b191
Ginger Barham <gingermostly@gmail.com>
2019-03-31 00:16:39 -0700

    Add schema.sql file to commit, set to ignored in error

---
+++ schema.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS bookshelf;
+
+USE bookshelf;
+
+CREATE TABLE IF NOT EXISTS authors (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(50) NOT NULL,
+  details varchar(2000) NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE IF NOT EXISTS books (
+  id int NOT NULL AUTO_INCREMENT,
+  title varchar(50) NOT NULL,
+  description varchar(2000) NOT NULL,
+  published_year int,
+  PRIMARY KEY (ID)
+);
