5cb743f01e898d5c776aa3faf974ead394737e46
Ginger Barham <gingermostly@gmail.com>
2019-04-02 11:36:26 -0700

    Add error handling to mysql connection

--- database/index.js
+++ database/index.js
@@ -1,31 +1,32 @@
 const mysql = require('mysql');
 const config = require('./config');
+
 const connection = mysql.createConnection(config);
 
-connection.connect();
+connection.connect(function(err){
+  if(err) throw err;
+  console.log('connected')
+});
 
-const search = (search, values) => {
-  return new Promise((resolve, reject) => {
-    connection.query(search, values, (err, insert) => {
-      if(err)
-      return reject(err);
-      resolve(insert)
-    })
-  })
+const search = (search, values) => new Promise((resolve, reject) => {
+  connection.query(search, values, (err, insert) => {
+    if (err) return reject(err);
+    resolve(insert);
+  });
+});
+
+const addBook = function (value) {
+  return search('INSERT INTO books (title, author, author_details, description, published_year) VALUES (?, ?, ?, ?, ?)', [value.title, value.author, value.author_details, value.description, value.published_year]);
 };
 
-const addBook = function(value) {
-  return search ('INSERT INTO books (title, author, author_details, description, published_year) VALUES (?, ?, ?, ?, ?)', [value.title, value.author, value.author_details, value.description, value.published_year])
-}
+const getBook = function (id) {
+  return search(`SELECT * FROM books WHERE id =${id}`);
+};
 
-const getBook = function(id) {
-  return search('SELECT * FROM books WHERE id =' + id);
-}
-
-const close = function() {
+const close = function () {
   connection.end();
-}
+};
 
 module.exports = {
-  addBook, getBook, close
-}
+  addBook, getBook, close,
+};
