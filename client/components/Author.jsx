4b0fd20e91a878da948375c263155d7b99601182
Ginger Barham <gingermostly@gmail.com>
2019-04-03 19:22:35 -0700

    Add font styling, width and border styled components

--- client/components/Author.jsx
+++ client/components/Author.jsx
@@ -1,4 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const About = styled.h1`
+  font-family: lato, Helvetica Neue, Helvetica, Arial, sans-serif;
+  font-size: 12px;
+  color: #382110;
+  text-transform: uppercase;
+  border-bottom: 1px solid #D8D8D8;
+`;
+
+const H2 = styled.h2`
+  font-family: merriweather, serif;
+`;
+
+const Container = styled.div`
+  float: right;
+  display: inline;
+  width: 300px;
+`;
 
 class Author extends React.Component {
   constructor(props) {
@@ -7,6 +26,7 @@ class Author extends React.Component {
       authors: []
     }
   }
+
   componentDidMount() {
     fetch('http://127.0.0.1:3000/books/1/authors/1')
     .then((res) => {
@@ -20,21 +40,24 @@ class Author extends React.Component {
   }
   render() {
     return (
-      <div className="author">
+      <Container>
         {this.state.authors.map(item => {
-          console.log(item)
-          return <div>
-            <h2>About {item.name}</h2>
+          return (
+            <div>
+            <About>
+              About {item.name}
+              </About>
             <img src={item.profile_pic}></img>
-            <h3>{item.name}</h3>
+            <H2>
+            {item.name}
+            </H2>
             <button>Follow Author</button>
             <p>{item.details}</p>
-          </div>
+            </div>
+          )
         })}
-      </div>
-
+      </Container>
     )
-
   }
 }
 
