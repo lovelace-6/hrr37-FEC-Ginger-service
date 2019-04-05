ffd500bc04abe907bf6400a9eb00b26dbda657ba
Ginger Barham <gingermostly@gmail.com>
2019-04-05 02:00:04 -0700

    Setup basic test using enzyme/jest

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Author from '../components/Author.jsx';
+import App from '../components/App.jsx'
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Author />
-      </div>
-    );
-  }
-}
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
