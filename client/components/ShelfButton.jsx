8edf79e68fb386d98b385fe19d359b6cea4df172
Ginger Barham <gingermostly@gmail.com>
2019-04-05 14:14:06 -0700

    Create component for dropdown and choose shelf button

---
+++ client/components/ShelfButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ShelfButton extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default ShelfButton;
