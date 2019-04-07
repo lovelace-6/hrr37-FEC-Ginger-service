e27d664e5804b0fd4e0524c30f1c57a0f7d1aaac
Ginger Barham <gingermostly@gmail.com>
2019-04-07 12:29:15 -0700

    Added ShelfList component

--- client/components/ShelfButton.jsx
+++ client/components/ShelfButton.jsx
@@ -22,19 +22,49 @@ const DropDownButton = styled.button`
   background-color: #409D69;
 `;
 
+const DropDownList = styled.ul`
+  font-family: Lato, Helvetica Neue, Arial, sans-serif;
+  font-size: 12px;
+  list-style-type: none;
+`;
+
+const DropDownContainer = styled.div`
+  max-height: 215px;
+  margin: 2px 0px 0px 0px;
+  overflow-x: hidden;
+  overflow-y: auto;
+  padding-bottom: 2px;
+`;
+
 class ShelfButton extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isHidden: true
+    }
   }
   render() {
     return (
       <div>
         <form>
+        <DropDownContainer>
         <MainButton>
           Want to Read
         </MainButton>
         <DropDownButton>
         </DropDownButton>
+        <DropDownList>
+          <li>
+            Read
+          </li>
+          <li>
+            Currently Reading
+          </li>
+          <li>
+            currently-reading-again
+          </li>
+        </DropDownList>
+        </DropDownContainer>
         </form>
       </div>
     )
---
+++ client/components/ShelfList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+import ShelfButton from '../components/ShelfButton.jsx';
+
+class ShelfList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+
+  }
+}
+
+export default ShelfList;
