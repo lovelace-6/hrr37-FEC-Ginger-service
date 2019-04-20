d7dd1498568bba16df62da8c78fea24146801eac
Max Hsu <maxhsu1@gmail.com>
2019-04-19 20:26:56 -0700

    removed explicitly including column id in data gen

--- psql.schema.sql
+++ psql.schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS authors (
 
 );
 
--- copies cvs file to postgres
+--copies cvs file to postgres
 \copy books(title,description,author_id, published_year, cover, status) FROM '/Users/Maxwell/Desktop/hrr37-FEC-Ginger-service/BooksData.csv' DELIMITER ',' CSV HEADER;
 
 \copy authors(name, details, profile_pic, followers) FROM '/Users/Maxwell/Desktop/hrr37-FEC-Ginger-service/AuthorsData.csv' DELIMITER ',' CSV HEADER;
