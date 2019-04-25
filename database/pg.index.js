7130d11ea3de3cfd463a48165bfdeb3d33e4bf99
Max Hsu <maxhsu1@gmail.com>
2019-04-24 21:45:41 -0700

    added delete and Add crud functions

--- database/pg.index.js
+++ database/pg.index.js
@@ -61,9 +61,39 @@ var updateStatus = async (bookStatus, booksId) =>{
   }
 }
 
+//EXTENDED FEATURES OF CRUD
+var deleteBook = async (id) =>{
+  console.log('pg received')
+  try{
+   var query = `delete from books where id =${id}`
+   var response = await pool.query(query)
+   console.log(response)
+
+  } catch (error){
+    return error
+  }
+}
+
+var addBook = async (value)=>{
+  try{
+    console.log('title', typeof value.title)
+    var query =`insert into books(title,description, author_id, published_year, cover, status) VALUES ('${value.title}', '${value.description}', ${value.author_id}, ${value.published_year}, '${value.cover}', '${value.status}')`
+
+    var response = await pool.query(query)
+
+    console.log(response)
+
+  } catch(error){
+    return error
+  }
+}
+
 module.exports = {
   getBooks:getBooks,
   getAuthor:getAuthor,
   getAuthorTitles:getAuthorTitles,
-  updateStatus, updateStatus
+  updateStatus: updateStatus,
+  addBook: addBook,
+  deleteBook:deleteBook
+
 }
