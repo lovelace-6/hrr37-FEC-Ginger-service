f360d79fbbbb927e4edd38553dbaf82dc1efe55d
Max Hsu <maxhsu1@gmail.com>
2019-04-24 21:45:20 -0700

    added delete and add crud functions

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const port = 3000;
 const path = require('path');
 const bodyParser = require('body-parser');
 const pg = require('../database/pg.index.js')
+const data = require('../database/dummy_data.js')
+
 // const mongo = require('../database/mongo.index.js')
 
 app.use(bodyParser.json());
@@ -12,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/books/:id', express.static(path.join(__dirname, '../public')));
 
-//MONGO VERSION
+//USING POSTGRES
 
 app.get('/books/:id/authors/title', async (req, res) => {
     console.log('server/index line 16')
@@ -38,7 +40,7 @@ app.get('/books/:id/authors/:id/titles', async (req, res) => {
   res.json(books);
 });
 
-
+//EXTENDING CRUD OPERATIONS TO ADD AND DELTE
 app.post('/books/:id/authors/status', async (req, res) => {
   var bookStatus = req.body.status;
   var booksId = req.body.id
@@ -46,5 +48,26 @@ app.post('/books/:id/authors/status', async (req, res) => {
   (bookStatus, booksId);
   res.send(status);
 });
+
+app.post('/books/:id/addBook', async (req,res)=>{
+  console.log('added request triggered')
+  var value = {
+    title: data.title(),
+    description: data.description(),
+    author_id: data.author_id({min:1, max:1000000}),
+    published_year: data.year({ min: 1920, max: 2019}),
+    cover: data.cover + data.author_id({min:1,max: 7})+'.jpg',
+    status: data.status
+  }
+  console.log('new book data',value)
+  var response = await pg.addBook(value)
+  console.log(response)
+})
+
+app.delete('books/:id/deleteBook', async(req,res)=>{
+  console.log('delete triggered')
+  var response = await pg.deleteBook(req.params.id)
+  console.log(response)
+})
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
