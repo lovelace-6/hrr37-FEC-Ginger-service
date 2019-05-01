bc91629d830dbd292d62ead5d602af355a387f7f
Max Hsu <maxhsu1@gmail.com>
2019-05-01 16:39:54 -0700

    styling changes

--- database/psql.seed.js
+++ database/psql.seed.js
@@ -4,10 +4,9 @@ const fs = require('fs');
 const data = require('../database/dummy_data.js')
 
 
-const generateBooks = (id) =>{
+const generateBooks = () =>{
 //note to self, added booksID field for mongodb to avoid using objectID
   var books = {
-    booksID: id,
     title: data.title(),
     description: data.description(),
     author_id: data.author_id({min:1, max:1000000}),
@@ -19,10 +18,9 @@ const generateBooks = (id) =>{
 }
 
 //added authorID for mongodb specific use case
-const generateAuthor = (id) => {
+const generateAuthor = () => {
 
  var authors = {
-    authorsID: id,
     name: data.name(),
     details: data.details(),
     profile_pic: data.profilePic(),
@@ -32,7 +30,7 @@ const generateAuthor = (id) => {
 }
 
 
-//Writing CSV file
+// Writing CSV file
 var csvStream = csv.createWriteStream({headers: true})
 var writableStream = fs.createWriteStream("BooksData.csv");
 
@@ -50,7 +48,7 @@ var numOfBooks = 1;
 function writeBooks(){
 
 while( numOfBooks<10000001){
-if(!csvStream.write(generateBooks(numOfBooks))){
+if(!csvStream.write(generateBooks())){
   numOfBooks++
   return
   }
@@ -66,36 +64,36 @@ var start = new Date()
 writeBooks()
 
 
-var csvStreamAuthors = csv.createWriteStream({headers: true})
-var writableStreamAuthors = fs.createWriteStream("AuthorsData.csv");
+// var csvStreamAuthors = csv.createWriteStream({headers: true})
+// var writableStreamAuthors = fs.createWriteStream("AuthorsData.csv");
 
-writableStreamAuthors.on("finish", function(){
-  console.log("Authors DONE!");
-});
+// writableStreamAuthors.on("finish", function(){
+//   console.log("Authors DONE!");
+// });
 
-csvStreamAuthors.pipe(writableStreamAuthors);
-csvStreamAuthors.on('drain', function() {
-  writeAuthors();
-});
-var numOfAuthors =1;
+// csvStreamAuthors.pipe(writableStreamAuthors);
+// csvStreamAuthors.on('drain', function() {
+//   writeAuthors();
+// });
+// var numOfAuthors =1;
 
-function writeAuthors(){
+// function writeAuthors(){
 
-while( numOfAuthors<1000001){
-if(!csvStreamAuthors.write(generateAuthor(numOfAuthors))){
-  numOfAuthors++
-  return
-  }
-numOfAuthors++
-}
-var endAuthors = new Date() - startAuthors
-console.log('time required to generate CSV',endAuthors, 'ms')
-csvStreamAuthors.end();
+// while( numOfAuthors<1000001){
+// if(!csvStreamAuthors.write(generateAuthor())){
+//   numOfAuthors++
+//   return
+//   }
+// numOfAuthors++
+// }
+// var endAuthors = new Date() - startAuthors
+// console.log('time required to generate CSV',endAuthors, 'ms')
+// csvStreamAuthors.end();
 
-}
+// }
 
-var startAuthors = new Date()
-writeAuthors()
+// var startAuthors = new Date()
+// writeAuthors()
 
 
 
